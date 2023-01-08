a4b55780f972e57d940925130ef6fbcd713bd632
DHRUVRAJSINH <dhruvsss007@gmail.com>
2023-01-08 11:16:16 +0530

    Navbar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,38 @@
+import { Paper,IconButton } from '@mui/material'
+import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import { useNavigate } from 'react-router-dom'
+const Searchbar = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!searchTerm)
+      navigate(`/search/${searchTerm}`);
+     setSearchTerm(''); 
+  }
+  return (
+    <Paper
+      component='form'
+      onSubmit={handleSubmit}
+      sx={{
+        borderRadius: 20, 
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr:{sm:5},
+      }}
+    >
+    <input
+      className='search-bar'
+      placeholder='Search what you want to watch'
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+    />  
+     <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+        <SearchIcon/>
+      </IconButton>
+    </Paper>
+  )
+}
+export default Searchbar
---
+++ src/components/VideoDetail.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const VideoDetail=()=> {
+  return (
+    <div>Videodetail</div>
+  )
+}
+
+export default VideoDetail
