8c9414816cc19f6742bb50093097a9ecf9b822b9
DHRUVRAJSINH <dhruvsss007@gmail.com>
2023-06-13 15:05:25 +0530

    Fixed the issue of YT-API

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,35 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import { Box, Stack, Typography } from '@mui/material'
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 import { FetchApi } from "../utils/FetchApi";
 import { Videos, Sidebar } from "./";
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
   const [videos, setVideos] = useState(null);
-  // console.log(videos);
   useEffect(() => {
     setVideos(null);
 
-    FetchApi(`search?part=snippet&q=${selectedCategory}`)
-      .then((data) => setVideos(data.items))
+    FetchApi(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
   }, [selectedCategory]);
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
       </Box>
 
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "silver" }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "silver" }}
+        >
           {selectedCategory} <span style={{ color: "Red" }}>Videos</span>
         </Typography>
         <Videos videos={videos} />
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          copyright © {new Date().getFullYear()} by <span style={{ color: "#FC1503" }}>Youtube</span>. All rights reserved.
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          copyright © {new Date().getFullYear()} by{" "}
+          <span style={{ color: "#FC1503" }}>Youtube</span>. All rights
+          reserved.
         </Typography>
       </Box>
     </Stack>
   );
 };
 
-export default Feed
+export default Feed;
